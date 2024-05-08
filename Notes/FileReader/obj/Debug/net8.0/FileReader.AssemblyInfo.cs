f1c918c849a597256b29941458d41523aed87d7d
paktozi <130216112+paktozi@users.noreply.github.com>
2024-05-08 14:52:27 +0300

    Fix

--- Notes/FileReader/obj/Debug/net8.0/FileReader.AssemblyInfo.cs
+++ Notes/FileReader/obj/Debug/net8.0/FileReader.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FileReader")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdabb4fbcc9482834116946a3738043c648b9885")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ed5cd4df1d69c723308bab772887f75f81ecc3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("FileReader")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FileReader")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
